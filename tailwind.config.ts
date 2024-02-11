f0d62f3b1c3e9a914b02e188aaac3452a6746a28
katsuwoo <kty.h0624@gmail.com>
2024-02-11 22:05:09 +0900

    modify

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero": "url('/hero.webp')",
+      },
+      colors: {
+        'main-blue': '#01345b',
+        'sub-blue': '#5ce0d8',
+        'sub-yellow': '#ffcf43',
       },
     },
   },
